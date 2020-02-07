bb29edba2766a753b3b9e19dc62e5a982fbbd12c
Israelmath <60768876+Israelmath@users.noreply.github.com>
2020-02-07 19:34:37 -0300

    O alvorecer dos pássaros
    Até agora, com bastante dificuldade, consegui criar uma tela. A imagem acima é de um repositório que já tem o jogo pronto.

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,1 +1,21 @@
 import pygame
+from pygame.locals import *
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 800
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    # Define o tamanho(resolução) da tela
+
+BACKGROUND = pygame.image.load('background-day.png')
+BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT) )
+
+while True:
+
+    for event in pygame.event.get():                              # Laço principal
+        if event.type == QUIT:
+            pygame.quit()
+    pygame.display.update()
+
+    screen.blit(BACKGROUND, (0, 0))                               # A função blit coloca uma imagem em cima de outra
+    
