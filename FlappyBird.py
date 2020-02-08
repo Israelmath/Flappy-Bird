5bfc3eb3132f7cb5138978a5271bba7e1eff77f7
Israelmath <60768876+Israelmath@users.noreply.github.com>
2020-02-08 14:23:49 -0300

    Now, alone... Agora, sozinho
    Com algum esforço (nem foi tanto assim :-P), consegui adicionar pontuação ao jogo e um placar que conta a pontuação.

--- FlappyBird.py
+++ FlappyBird.py
@@ -82,11 +82,67 @@ class Pipe(pygame.sprite.Sprite):
         self.rect[0] -= GAMESPEED
 
 
+class Placar(pygame.sprite.Sprite):
+
+    def __init__(self, number, xpos):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.images = [pygame.image.load('0.png').convert_alpha(),
+                       pygame.image.load('1.png').convert_alpha(),
+                       pygame.image.load('2.png').convert_alpha(),
+                       pygame.image.load('3.png').convert_alpha(),
+                       pygame.image.load('4.png').convert_alpha(),
+                       pygame.image.load('5.png').convert_alpha(),
+                       pygame.image.load('6.png').convert_alpha(),
+                       pygame.image.load('7.png').convert_alpha(),
+                       pygame.image.load('8.png').convert_alpha(),
+                       pygame.image.load('9.png').convert_alpha()]
+
+
+        self.image = self.images[number]
+        self.rect = self.image.get_rect()
+        #print(self.images.get_rect())
+        self.rect[0] = xpos
+        self.rect[1] = 10
+
+
+
+    def update(self, score, placar):
+        score = str(score)
+
+        if len(score) == 1:
+            placar.sprites()[0].image = self.images[int(score[0])]
+        elif len(score) == 2:
+            placar.sprites()[0].image = self.images[int(score[0])]
+            placar.sprites()[1].image = self.images[int(score[1])]
+        else:
+            placar.sprites()[0].image = self.images[int(score[0])]
+            placar.sprites()[1].image = self.images[int(score[1])]
+            placar.sprites()[2].image = self.images[int(score[2])]
+
+
+            #self.image = self.images[score_count(score)]
+
+
+
+
 def is_off_screen(sprite):
+    """
+    Função que calcula se um sprite saiu da tela
+
+    :param sprite: Parâmetro que pode ser um cano ou um 'chão'.
+    :return: True ou False se o sprite saiu ou não da tela.
+    """
     return sprite.rect[0] < - sprite.rect[2]
 
 
 def get_random_pipes(xpos):
+    """
+    Função que retorna um par de canos em uma altura aleatória, contando com o espaço entre eles para o
+    pássaro passar
+    :param xpos: Posição, em x, do cano a ser gerado
+    :return: Tupla com um par de canos. Um virado para cima e o outro invertido
+    """
     size = random.randint(100, 400)
     pipe = Pipe(False, xpos, size)
     pipe_inverted = Pipe(True, xpos, SCREEN_HEIGHT - size - PIPE_GAP)
@@ -103,32 +159,63 @@ GROUND_HEIGHT = 100
 PIPE_WIDHT = 80
 PIPE_HEIGHT = 500
 PIPE_GAP = 200
-DIST = 1.5
+DIST = 1.9
+PLACAR_WIDTH = 24
+PLACAR_HEIGHT = 36
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    # Define o tamanho(resolução) da tela
 
 BACKGROUND = pygame.image.load('background-day.png')
-BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT) )
+BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
+"""
+Abaixo temos as linhas que criam, um grupo dos pássaros, criação
+e atribuição de um pássaro e adição desse pássaro ao grupo dos pássaros
+"""
 bird_group = pygame.sprite.Group()
 bird = Bird()
 bird_group.add(bird)
 
+"""
+Abaixo temos as linhas que criam, um grupo dos 'chãos', criação e atribuição de um
+ 'chão' já com a sua posição. Dentro do 'for' é criado mais um chão e adicionado ao
+ grupo porque depois excluiremos um deles e adicionaremos outro a uma fila infinita depois de 
+ checar se um deles saiu da tela.
+"""
 ground_group = pygame.sprite.Group()
 for i in range(2):
     ground = Ground(GROUND_WIDTH*i)
     ground_group.add(ground)
 
-pipe_group = pygame.sprite.Group()
 
+"""
+Abaixo temos as linhas que criam, um grupo dos canos, uma função, dentro do 'for' para criar
+dois pares de canos em alturas aleatórias pelo mesmo motivo dos chãos. 
+"""
+pipe_group = pygame.sprite.Group()
 i = 0
 for i in range(2):
     pipes = get_random_pipes(SCREEN_WIDTH*i + 800)
     pipe_group.add(pipes[0])
     pipe_group.add(pipes[1])
 
+
+"""
+Abaixo temos a linha que cria o placar
+"""
+score_group = pygame.sprite.Group()
+score = []
+for i in range(3):
+    placar = Placar(0, SCREEN_WIDTH-PLACAR_WIDTH*(i+1) - 10)
+    score.append(placar)
+score_group.add(score[0])
+score_group.add(score[1])
+score_group.add(score[2])
+
 clock = pygame.time.Clock()                                       # Essa classe diminui os fps junto com a clock.tick()
+
+pontos = 0
 
 while True:
     clock.tick(30)
@@ -157,20 +244,26 @@ while True:
         pipe_group.add(pipes[0])
         pipe_group.add(pipes[1])
 
+    if pipe_group.sprites()[0].rect[0] == SCREEN_WIDTH/2:
+        #print(pontos)
+        pontos += 1
+
     bird_group.update()
     pipe_group.update()
+    ground_group.update()
+    score_group.update(pontos, score_group)
+    score_group.draw(screen)
     bird_group.draw(screen)
     pipe_group.draw(screen)
+    ground_group.draw(screen)
+
+    pygame.display.update()
 
     # if pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask):
     #     # Game Over
     #     break
 
-    ground_group.update()
-    ground_group.draw(screen)
 
-    pygame.display.update()
-
-    if pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask) or pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask):
+    if pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask) or  pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask):
         # Game Over
         break
